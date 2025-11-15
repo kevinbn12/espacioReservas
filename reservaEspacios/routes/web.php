f5d449cac71dea30c64e1379e01f920f5d881f16
1201kevBN <bohorquezkevin12@gmail.com>
2025-11-15 14:20:11 -0500

    creacion de rutas

--- reservaEspacios/routes/web.php
+++ reservaEspacios/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EspacioController;
+use App\Http\Controllers\ReservaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('espacios', EspacioController::class);
+Route::resource('reservas', ReservaController::class);
